3b5ed11f291e6783715b96a6fed5e2367e5d8761
tasa-GitHub <86187897+tasa-GitHub@users.noreply.github.com>
2023-06-29 05:10:09 +0000

    merge title container to page

--- app/title_container.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./title_container.module.scss"
-
-export default function TitleContainer() {
-    return (
-        <div className={styles.wrapper}>
-            <h1>宇宙船からの脱出</h1>
-        </div>
-    )
-}
