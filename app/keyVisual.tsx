5e43252852e9084177817a48983365c74e638a82
polskaa <sen.0128.111@gmail.com>
2023-07-15 18:56:20 +0900

    change keyVisual image

--- app/keyVisual.tsx
+++ app/keyVisual.tsx
@@ -1,45 +1,61 @@
 import styles from "./keyVisual.module.scss";
 import { Sawarabi_Gothic } from "next/font/google";
+import Image from "next/image";
 
 const sawaGothic = Sawarabi_Gothic({ subsets: ["latin"], weight: "400" });
 
 export default function KeyVisual() {
   return (
     <>
-      <>
-        <div
-          style={{
-            position: "relative",
-            width: "100%",
-            height: 0,
-            paddingTop: "56.2225%",
-            paddingBottom: 0,
-            boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
-            marginTop: "1.6em",
-            marginBottom: "0.9em",
-            overflow: "hidden",
-            borderRadius: 8,
-            willChange: "transform",
-          }}
-        >
-          <iframe
-            loading="lazy"
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              top: 0,
-              left: 0,
-              border: "none",
-              padding: 0,
-              margin: 0,
-            }}
-            src="https://www.canva.com/design/DAFn6c6UIbk/watch?embed"
-            allowFullScreen="allowfullscreen"
-            allow="fullscreen"
-          ></iframe>
+      <div className={styles.container}>
+        <div className={styles.container_left}>
+          <h1 className={styles.title}>
+            墜落する
+            <br />
+            宇宙船
+            <br />
+            からの脱出
+          </h1>
+          <div className={styles.meteor}>
+            <Image
+              src="/meteor.gif"
+              alt="Meteor"
+              fill={true}
+              style={{ objectFit: "contain", zIndex: -1 }}
+            />
+          </div>
         </div>
-      </>
+        <div className={styles.container_right}>
+          <Image
+            src="/chikyuu.svg"
+            alt="Earth"
+            width={400}
+            height={400}
+            className={styles.earth}
+          />
+          <Image
+            src="/mars.svg"
+            alt="Mars"
+            width={400}
+            height={400}
+            className={styles.mars}
+          />
+          <Image
+            src="/tsubu.webp"
+            alt="tsubu"
+            width={300}
+            height={200}
+            className={styles.tsubu}
+          />
+          <Image
+            src="/rocket.svg"
+            alt="Rocket"
+            width={150}
+            height={150}
+            className={styles.rocket}
+          />
+        </div>
+      </div>
     </>
   );
 }
