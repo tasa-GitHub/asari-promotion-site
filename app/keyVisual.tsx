b347ef86a447a0dfaee7473cfb8acea25d7b3f51
polskaa <sen.0128.111@gmail.com>
2023-07-15 10:48:29 +0900

    add iframe

--- app/keyVisual.tsx
+++ app/keyVisual.tsx
@@ -3,18 +3,43 @@ import { Sawarabi_Gothic } from "next/font/google";
 
 const sawaGothic = Sawarabi_Gothic({ subsets: ["latin"], weight: "400" });
 
-export default function keyVisual() {
+export default function KeyVisual() {
   return (
     <>
-      <div className={sawaGothic.className}>
-        <div className={styles.keyVisual}>
-          <h1>
-            墜落する
-            <br />
-            宇宙船からの<span className={styles.highlight}>脱出</span>
-          </h1>
+      <>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: 0,
+            paddingTop: "56.2225%",
+            paddingBottom: 0,
+            boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+            marginTop: "1.6em",
+            marginBottom: "0.9em",
+            overflow: "hidden",
+            borderRadius: 8,
+            willChange: "transform",
+          }}
+        >
+          <iframe
+            loading="lazy"
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              left: 0,
+              border: "none",
+              padding: 0,
+              margin: 0,
+            }}
+            src="https://www.canva.com/design/DAFn6c6UIbk/watch?embed"
+            allowFullScreen="allowfullscreen"
+            allow="fullscreen"
+          ></iframe>
         </div>
-      </div>
+      </>
     </>
   );
 }
